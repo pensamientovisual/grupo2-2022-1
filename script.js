3b9e20db8ece15b83b8127e8d9d0e416cfab9bd0
Isidora Plummer <isidora.plummer@uc.cl>
2022-04-26 00:13:15 -0400

    js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-$(document).ready(function(){}
+$(document).ready(function(){
+    var inicio=$("#titulo");
+    inicio.mouseenter(function(){
+        $(this).fadeTo("slow",0);
+        $(this).fadeTo("slow",1);
+
+
+}
