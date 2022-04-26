4958fdf7f6154ded3145ac02bd76fee692eb0c0d
Ignacia Romero <ignaciaromero@MacBook-Air-de-Ignacia.local>
2022-04-26 01:04:04 -0400

    botones

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    var d4j = $("#biologia");
+
+    d4j.click(function(){
+        $(this).fadeTo("fast", 0);
+    });
+  
+    d4j.click(function(){
+        $(this).fadeTo("fast",1);
+    });
+  
+    d4j.click(function(){
+        $(d9j).show('fast');
+    });
+  
+    d4j.click(function(){
+      $(d12j).hide('fast');
+  });
+  })
