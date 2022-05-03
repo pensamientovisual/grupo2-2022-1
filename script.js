7fe20fcf5c0cca2dda96c7c118e335be8072ddbf
Isidora Plummer <isidora.plummer@uc.cl>
2022-05-02 23:29:13 -0400

    botones

--- script.js
+++ script.js
@@ -3,20 +3,12 @@ $(document).ready(function(){
     var bs = $("#boton1");
 
     bs.click(function(){
-        $(this).fadeTo("fast", 0);
+        $(this).fadeTo("slow", 0);
     });
   
     bs.click(function(){
-        $(this).fadeTo("fast",1);
+        $(this).fadeTo("slow",1);
     });
-  
-    bs.click(function(){
-        $(d9j).show('fast');
-    });
-  
-    bs.click(function(){
-      $(d12j).hide('fast');
-  });
 
 
   })
