0f89cf024779166a58b09566e0a4b134258df8b9
Ignacia Romero <ignaciaromero@MacBook-Air-de-Ignacia.local>
2022-05-02 23:41:47 -0400

    mensaje

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ $(document).ready(function(){
     var bs = $("#boton1");
 
     bs.click(function(){
+        $(this).fadeTo("fast", 0);
+        $(this).fadeTo("fast",1);
+    });
+
+    var bs = $("#boton2");
+
+    bs.click(function(){
+        $(this).fadeTo("slow", 0);
+        $(this).fadeTo("slow",1);
+    });
+
+    var bs = $("#boton3");
+
+    bs.click(function(){
+        $(this).fadeTo("slow", 0);
+        $(this).fadeTo("slow",1);
+    });
+
+    var bs = $("#boton4");
+
+    bs.click(function(){
         $(this).fadeTo("slow", 0);
         $(this).fadeTo("slow",1);
     });
