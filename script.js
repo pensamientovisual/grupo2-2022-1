ef0bd6532d8a1d08a428209beb7f0a89bb26e1f5
Ignacia Romero <ignaciaromero@MacBook-Air-de-Ignacia.local>
2022-05-07 13:17:33 -0400

    botones

--- script.js
+++ script.js
@@ -11,33 +11,31 @@ $(document).ready(function(){
     var bs = $("#boton1");
 
     bs.click(function(){
-        $(this).fadeTo("fast", 0);
-        $(this).fadeTo("fast",1);
+        $(this).fadeTo("fast", 0.5);
+        $(this).fadeTo("fast", 1);
     });
     
 
     var bs = $("#boton2");
 
     bs.click(function(){
-        $(this).fadeTo("slow", 0);
-        $(this).fadeTo("slow",1);
+        $(this).fadeTo("fast", 0.5);
+        $(this).fadeTo("fast",1);
     });
 
     var bs = $("#boton3");
 
     bs.click(function(){
-        $(this).fadeTo("slow", 0);
-        $(this).fadeTo("slow",1);
+        $(this).fadeTo("fast", 0.5);
+        $(this).fadeTo("fast",1);
     });
 
     var bs = $("#boton4");
 
     bs.click(function(){
-        $(this).fadeTo("slow", 0);
-        $(this).fadeTo("slow",1);
+        $(this).fadeTo("fast", 0.5);
+        $(this).fadeTo("fast",1);
     });
-
-
   })
 
 
@@ -46,8 +44,14 @@ function myFunction() {
     var x = document.getElementById("myDIV");
     if (x.style.display === "none") {
       x.style.display = "block";
+      $('#boton2').hide('fast');
+      $('#boton3').hide('fast');
+      $('#boton4').hide('fast');
     } else {
       x.style.display = "none";
+      $('#boton2').show('fast');
+      $('#boton3').show('fast');
+      $('#boton4').show('fast');
     }
 }
 
@@ -55,26 +59,44 @@ function myFunction2() {
     var x = document.getElementById("myDIV2");
     if (x.style.display === "none") {
       x.style.display = "block";
+      $('#boton1').hide('fast');
+      $('#boton3').hide('fast');
+      $('#boton4').hide('fast');
     } else {
       x.style.display = "none";
+      $('#boton1').show('fast');
+      $('#boton3').show('fast');
+      $('#boton4').show('fast');
     }
 }
 
 function myFunction3() {
     var x = document.getElementById("myDIV3");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
+        $('#boton2').hide('fast');
+        $('#boton1').hide('fast');
+        $('#boton4').hide('fast');
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
+        $('#boton2').show('fast');
+        $('#boton1').show('fast');
+        $('#boton4').show('fast');
     }
 }
 
 function myFunction4() {
     var x = document.getElementById("myDIV4");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
+        $('#boton2').hide('fast');
+        $('#boton3').hide('fast');
+        $('#boton1').hide('fast');
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
+        $('#boton2').show('fast');
+        $('#boton3').show('fast');
+        $('#boton1').show('fast');
     }
   }
 
