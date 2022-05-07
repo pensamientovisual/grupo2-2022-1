ea753376e61e82c30a8a21e0624d102f3fe7994b
Ignacia Romero <ignaciaromero@MacBook-Air-de-Ignacia.local>
2022-05-07 12:55:29 -0400

    botones

--- script.js
+++ script.js
@@ -42,3 +42,39 @@ $(document).ready(function(){
 
 
 
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("myDIV2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById("myDIV3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function myFunction4() {
+    var x = document.getElementById("myDIV4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
