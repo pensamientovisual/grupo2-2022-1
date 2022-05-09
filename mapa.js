d047caacf48380b161dc07a1274a2d0fd0b29ee9
Javiera Martinez <javieramartinez@MacBook-Pro-de-Javiera.local>
2022-05-09 17:06:16 -0400

    mapa

---
+++ mapa.js
@@ -0,0 +1,379 @@
+$(document).ready(function(){
+    $(".hide").hide();
+
+    $(".n1").click(function(){
+        $(".hide").hide();
+        $(".n1-txt").fadeIn(300);
+    });
+
+      $(".n2").click(function(){
+        $(".hide").hide();
+      $(".n2-txt").fadeIn(300);
+     });
+
+      $(".n3").click(function(){
+        $(".hide").hide();
+      $(".n3-txt").fadeIn(300);
+     });
+
+      $(".n4").click(function(){
+        $(".hide").hide();
+      $(".n4-txt").fadeIn(300);
+     });
+
+      $(".n5").click(function(){
+        $(".hide").hide();
+      $(".n5-txt").fadeIn(300);
+     });
+
+      $(".n6").click(function(){
+        $(".hide").hide();
+      $(".n6-txt").fadeIn(300);
+     });
+
+      $(".n7").click(function(){
+        $(".hide").hide();
+      $(".n7-txt").fadeIn(300);
+     });
+
+      $(".n8").click(function(){
+        $(".hide").hide();
+      $(".n8-txt").fadeIn(300);
+     });
+
+      $(".n9").click(function(){
+        $(".hide").hide();
+      $(".n9-txt").fadeIn(300);
+     });
+
+      $(".n10").click(function(){
+        $(".hide").hide();
+      $(".n10-txt").fadeIn(300);
+     });
+
+      $(".n11").click(function(){
+        $(".hide").hide();
+      $(".n11-txt").fadeIn(300);
+     });
+
+      $(".n12").click(function(){
+        $(".hide").hide();
+      $(".n12-txt").fadeIn(300);
+     });
+
+      $(".n13").click(function(){
+        $(".hide").hide();
+      $(".n13-txt").fadeIn(300);
+     });
+
+      $(".n14").click(function(){
+        $(".hide").hide();
+      $(".n14-txt").fadeIn(300);
+     });
+
+      $(".n15").click(function(){
+        $(".hide").hide();
+      $(".n15-txt").fadeIn(300);
+     });
+
+      $(".n16").click(function(){
+        $(".hide").hide();
+      $(".n16-txt").fadeIn(300);
+     });
+
+      $(".n17").click(function(){
+        $(".hide").hide();
+      $(".n17-txt").fadeIn(300);
+     });
+
+      $(".n18").click(function(){
+        $(".hide").hide();
+      $(".n18-txt").fadeIn(300);
+     });
+
+      $(".n19").click(function(){
+        $(".hide").hide();
+      $(".n19-txt").fadeIn(300);
+     });
+
+      $(".n20").click(function(){
+        $(".hide").hide();
+      $(".n20-txt").fadeIn(300);
+     });
+
+      $(".n21").click(function(){
+        $(".hide").hide();
+      $(".n21-txt").fadeIn(300);
+     });
+
+      $(".n22").click(function(){
+        $(".hide").hide();
+      $(".n22-txt").fadeIn(300);
+     });
+
+      $(".n23").click(function(){
+        $(".hide").hide();
+      $(".n23-txt").fadeIn(300);
+     });
+
+      $(".n24").click(function(){
+        $(".hide").hide();
+      $(".n24-txt").fadeIn(300);
+     });
+
+      $(".n25").click(function(){
+        $(".hide").hide();
+      $(".n25-txt").fadeIn(300);
+     });
+
+      $(".n26").click(function(){
+        $(".hide").hide();
+      $(".n26-txt").fadeIn(300);
+     });
+
+      $(".n27").click(function(){
+        $(".hide").hide();
+      $(".n27-txt").fadeIn(300);
+     });
+
+      $(".n28").click(function(){
+        $(".hide").hide();
+      $(".n28-txt").fadeIn(300);
+     });
+
+      $(".n29").click(function(){
+        $(".hide").hide();
+      $(".n29-txt").fadeIn(300);
+     });
+
+      $(".n30").click(function(){
+        $(".hide").hide();
+      $(".n30-txt").fadeIn(300);
+     });
+
+      $(".n31").click(function(){
+        $(".hide").hide();
+      $(".n31-txt").fadeIn(300);
+     });
+
+      $(".n32").click(function(){
+        $(".hide").hide();
+      $(".n32-txt").fadeIn(300);
+     });
+
+      $(".n33").click(function(){
+        $(".hide").hide();
+      $(".n33-txt").fadeIn(300);
+     });
+
+      $(".n34").click(function(){
+        $(".hide").hide();
+      $(".n34-txt").fadeIn(300);
+     });
+
+      $(".n35").click(function(){
+        $(".hide").hide();
+      $(".n35-txt").fadeIn(300);
+     });
+
+      $(".n36").click(function(){
+        $(".hide").hide();
+      $(".n36-txt").fadeIn(300);
+     });
+
+      $(".n37").click(function(){
+        $(".hide").hide();
+      $(".n37-txt").fadeIn(300);
+     });
+
+      $(".n38").click(function(){
+        $(".hide").hide();
+      $(".n38-txt").fadeIn(300);
+     });
+
+      $(".n39").click(function(){
+        $(".hide").hide();
+      $(".n39-txt").fadeIn(300);
+     });
+
+      $(".n40").click(function(){
+        $(".hide").hide();
+      $(".n40-txt").fadeIn(300);
+     });
+
+      $(".n41").click(function(){
+        $(".hide").hide();
+      $(".n41-txt").fadeIn(300);
+     });
+
+      $(".n42").click(function(){
+        $(".hide").hide();
+      $(".n42-txt").fadeIn(300);
+     });
+
+      $(".n43").click(function(){
+        $(".hide").hide();
+      $(".n43-txt").fadeIn(300);
+     });
+
+      $(".n44").click(function(){
+        $(".hide").hide();
+      $(".n44-txt").fadeIn(300);
+     });
+
+      $(".n45").click(function(){
+        $(".hide").hide();
+      $(".n45-txt").fadeIn(300);
+     });
+
+      $(".n46").click(function(){
+        $(".hide").hide();
+      $(".n46-txt").fadeIn(300);
+     });
+
+      $(".n47").click(function(){
+        $(".hide").hide();
+      $(".n47-txt").fadeIn(300);
+     });
+
+      $(".n48").click(function(){
+        $(".hide").hide();
+      $(".n48-txt").fadeIn(300);
+     });
+
+      $(".n49").click(function(){
+        $(".hide").hide();
+      $(".n49-txt").fadeIn(300);
+     });
+
+      $(".n50").click(function(){
+        $(".hide").hide();
+      $(".n50-txt").fadeIn(300);
+     });
+
+      $(".n51").click(function(){
+        $(".hide").hide();
+      $(".n51-txt").fadeIn(300);
+     });
+
+      $(".n52").click(function(){
+        $(".hide").hide();
+      $(".n52-txt").fadeIn(300);
+     });
+
+      $(".n53").click(function(){
+        $(".hide").hide();
+      $(".n53-txt").fadeIn(300);
+     });
+
+      $(".n54").click(function(){
+        $(".hide").hide();
+      $(".n54-txt").fadeIn(300);
+     });
+
+      $(".n55").click(function(){
+        $(".hide").hide();
+      $(".n55-txt").fadeIn(300);
+     });
+
+      $(".n56").click(function(){
+        $(".hide").hide();
+      $(".n56-txt").fadeIn(300);
+     });
+
+      $(".n57").click(function(){
+        $(".hide").hide();
+      $(".n57-txt").fadeIn(300);
+     });
+
+      $(".n58").click(function(){
+        $(".hide").hide();
+      $(".n58-txt").fadeIn(300);
+     });
+
+      $(".n59").click(function(){
+        $(".hide").hide();
+      $(".n59-txt").fadeIn(300);
+     });
+
+      $(".n60").click(function(){
+        $(".hide").hide();
+      $(".n60-txt").fadeIn(300);
+     });
+
+      $(".n61").click(function(){
+        $(".hide").hide();
+      $(".n61-txt").fadeIn(300);
+     });
+
+      $(".n62").click(function(){
+        $(".hide").hide();
+      $(".n62-txt").fadeIn(300);
+     });
+
+      $(".n63").click(function(){
+        $(".hide").hide();
+      $(".n63-txt").fadeIn(300);
+     });
+
+      $(".n64").click(function(){
+        $(".hide").hide();
+      $(".n64-txt").fadeIn(300);
+     });
+
+      $(".n65").click(function(){
+        $(".hide").hide();
+      $(".n65-txt").fadeIn(300);
+     });
+
+      $(".n66").click(function(){
+        $(".hide").hide();
+      $(".n66-txt").fadeIn(300);
+     });
+
+      $(".n67").click(function(){
+        $(".hide").hide();
+      $(".n67-txt").fadeIn(300);
+     });
+
+      $(".n68").click(function(){
+        $(".hide").hide();
+      $(".n68-txt").fadeIn(300);
+     });
+
+      $(".n69").click(function(){
+        $(".hide").hide();
+      $(".n69-txt").fadeIn(300);
+     });
+
+      $(".n70").click(function(){
+        $(".hide").hide();
+      $(".n70-txt").fadeIn(300);
+     });
+
+      $(".n71").click(function(){
+        $(".hide").hide();
+      $(".n71-txt").fadeIn(300);
+     });
+
+      $(".n72").click(function(){
+        $(".hide").hide();
+      $(".n72-txt").fadeIn(300);
+     });
+
+      $(".n73").click(function(){
+        $(".hide").hide();
+      $(".n73-txt").fadeIn(300);
+     });
+
+      $(".n74").click(function(){
+        $(".hide").hide();
+      $(".n74-txt").fadeIn(300);
+     });
+
+      $(".n75").click(function(){
+        $(".hide").hide();
+      $(".n75-txt").fadeIn(300);
+     });
+
+});
